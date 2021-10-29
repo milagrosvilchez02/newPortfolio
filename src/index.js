836448ccc6944918186f827aee16c13b29ca3054
Milagros Vilchez <milagrosvilchez02@gmail.com>
2021-10-29 18:05:20 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/newPortfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
