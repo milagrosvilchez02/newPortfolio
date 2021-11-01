5301d87c1db13a9dc7db85f937e14c0337822c60
Milagros Vilchez <milagrosvilchez02@gmail.com>
2021-11-01 11:33:48 -0300

    fixing link

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/newPortfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
